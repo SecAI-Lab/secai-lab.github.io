7fd1f14a132c054cda9a92a9b4b9a26a0fd30264
DenisKimskku <kor8821@naver.com>
2026-01-20 17:14:58 +0900

    Adding windows support for local tests (via powershell script), adding  domestic and workshop github links

---
+++ local_run.ps1
@@ -0,0 +1,3 @@
+# local_run.ps1
+Write-Host "Starting Jekyll site with force polling..." -ForegroundColor Cyan
+bundle exec jekyll serve --force_polling
--- local_run.sh
+++ local_run.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-bundle exec jekyll serve --force_polling --livereload 
+bundle exec jekyll serve --force_polling
